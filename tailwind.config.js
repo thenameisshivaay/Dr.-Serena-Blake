c381f03a85def4352baf6fa0c3353c0b1909a6e4
Abhishek Dixit <153361834+thenameisshivaay@users.noreply.github.com>
2025-07-06 10:04:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['"Playfair Display"', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        
+      },
+    },
+  },
+  plugins: [],
+};
